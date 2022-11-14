91ce239b16c9b858fff4d9b752cad626d59cc7bd
Stephen Thomas <stephen.thomas@buildingestimates.com>
2022-11-14 13:42:50 +1300

    Change package file format to accomodate env setup

--- conda_build_cdk/conda_build_cdk/conda_build_cdk_stack.py
+++ conda_build_cdk/conda_build_cdk/conda_build_cdk_stack.py
@@ -54,8 +54,7 @@ class CondaBuildCdkStack(Stack):
                     'goofys $conda_channel_bucket /mnt/channels',
                 ],
                 commands=[
-                    "conda install -c conda-forge boto3",
-                    "conda env update --file environment.lock.yml --prune",
+                    "conda install -c pytorch -c conda-forge -c fastai -c  --file environment.lock.yml",
                     "python setup.py bdist_conda",
                     "cp /opt/conda/conda-bld/linux-64/detectron2-* /mnt/channels/$conda_channel_name/linux-64/",
                     #"LINUX64_PACKAGES=/mnt/channels/$conda_channel_name/linux-64/*.tar.bz2 && conda convert -f --platform osx-64 ${LINUX64_PACKAGES} -o /mnt/channels/$conda_channel_name",
